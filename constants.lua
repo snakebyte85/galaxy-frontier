588dc7965633c935c48d0826c9faacb0839a48f0
snakebyte <snakebyte8@gmail.com>
2021-01-30 21:44:31 +0100

    Space

---
+++ constants.lua
@@ -0,0 +1,37 @@
+const={
+
+   screen={
+      max_x=128,
+      max_y=128
+   },
+
+   input={
+      left=0,
+      right=1,
+      up=2,
+      down=3,
+      o=4,
+      x=5
+   },
+
+   colors={
+      black=0,
+      dark_blue=1,
+      dark_purple=2,
+      dark_green=3,
+      brown=4,
+      dark_gray=5,
+      light_gray=6,
+      white=7,
+      red=8,
+      orange=9,
+      yellow=10,
+      green=11,
+      blue=12,
+      indigo=13,
+      pink=14,
+      peach=15
+   }
+
+
+}
---
+++ space.lua
@@ -0,0 +1,81 @@
+space={
+   speed_x=0,
+   speed_y=1,
+   stars_count=50,
+   stars={}
+}
+
+function create_star()
+      return {
+         x=0,
+         y=0,
+         distance=0         
+      }
+end
+
+function space_init()
+   for i=1,space.stars_count do
+      star = create_star()
+      star.x = flr(rnd(127))
+      star.y = flr(rnd(127))
+      star.distance=rnd(1)
+      add(space.stars,star)
+   end   
+end
+
+function space_draw()
+
+   for star in all(space.stars) do
+      color = const.colors.white
+      if star.distance>0.8 then
+         color=const.colors.dark_gray
+      end
+      pset(star.x,star.y,color)
+   end
+end
+
+
+function space_update()
+
+   stars_to_delete = {}
+
+   for i,star in ipairs(space.stars) do
+      star.x = star.x + (space.speed_x * (1-star.distance))
+      star.y = star.y + (space.speed_y * (1-star.distance))
+
+      if star.x > const.screen.max_x or star.x < 0 or         
+         star.y > const.screen.max_y or star.y < 0 then
+         add(stars_to_delete, i)
+      end
+   end
+
+   for i in all(stars_to_delete) do
+      deli(space.stars,i)
+   end
+
+   for i=1,#stars_to_delete do
+      star = create_star()
+      if space.speed_x == 0 then
+         star.x = flr(rnd(127))
+         if space.speed_y > 0 then
+            star.y = 0
+         else
+            star.y = const.screen.max_y
+         end
+      elseif space.speed_y == 0 then
+         star.y = flr(rnd(127))
+         if space.speed_x > 0 then
+            star.x = 0
+         else
+            star.x = const.screen.max_x
+         end
+      else 
+         star.x = flr(rnd(127))
+         star.y = flr(rnd(127))
+      end
+
+      star.distance=rnd(1)
+      add(space.stars,star)
+   end
+   
+end
